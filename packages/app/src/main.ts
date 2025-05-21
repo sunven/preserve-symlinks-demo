0c1026bb79effc1b1041259067ba414739d8598d
wenquansun <604164227@qq.com>
2025-05-21 12:27:07 +0800

    feat: deps

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,10 +1,7 @@
-import { omit } from 'lodash-es'
-import { util1, util2 } from 'utils'
-function fn1() {
-  return omit({ a: { a1: 1, a2: 2 } }, 'a.a1')
-}
+import // a,
+// defaultTo,
+'lodash-es'
+import { util1 } from 'utils'
 
-console.log('app omit', JSON.stringify(fn1()))
-console.log('utils omit', JSON.stringify(util1()))
-// console.log('app defalutTo', JSON.stringify(defalutTo(null, 2)))
-console.log('utils defalutTo', JSON.stringify(util2()))
+// console.log('app defaultTo', JSON.stringify(defaultTo(null, 2)))
+console.log('utils defaultTo', JSON.stringify(util1()))
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,7 +1,5 @@
-import { omit, defalutTo } from 'lodash-es'
+import { defaultTo } from 'lodash-es'
+
 export function util1() {
-  return omit({ a: { a1: 1, a2: 3111 } }, 'a.a1')
-}
-export function util2() {
-  return defalutTo(null, 3)
+  return defaultTo(null, 'util')
 }
